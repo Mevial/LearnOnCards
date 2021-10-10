3660db869f8ebc5c0a58d4b33c1c3a27bc888340
Max <61512470+Mevial@users.noreply.github.com>
2021-10-10 20:17:31 +0300

    add Provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 import {App} from "./App/App";
+import {Provider} from "react-redux";
+import {store} from "./App/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
