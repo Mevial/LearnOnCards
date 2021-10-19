0f1e1a8ed1ce8c6a81d02774433eb6c0df4eba4c
Max <61512470+Mevial@users.noreply.github.com>
2021-10-19 16:25:11 +0300

    update base url (neko) in setPassword

--- src/dal/setPasswordAPI.ts
+++ src/dal/setPasswordAPI.ts
@@ -4,7 +4,8 @@ const instanceHeroku = axios.create({
     baseURL: "https://neko-back.herokuapp.com/2.0/",
 })
 const instanceLocal = axios.create({
-    baseURL: "http://localhost:7542/2.0/",
+    // baseURL: "http://localhost:7542/2.0/",
+    baseURL: "https://neko-back.herokuapp.com/2.0/",
 })
 export const forgotPasswordAPI = {
     forgot(data: ForgotPasswordType) {
